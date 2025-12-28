d677f25b983487c5ff9de7aca2a7564249d81fc5
Tetrakern <26898880+Tetrakern@users.noreply.github.com>
2025-12-28 23:20:29 +0100

    Update chapter story ID helper to first check parent

--- includes/functions/_helpers-query.php
+++ includes/functions/_helpers-query.php
@@ -372,6 +372,12 @@ if ( get_option( 'fictioneer_disable_extended_story_list_meta_queries' ) ) {
  */
 
 function fictioneer_get_chapter_story_id( $chapter_id ) {
+  $chapter = get_post( $chapter_id );
+
+  if ( $chapter->post_parent ) {
+    return $chapter->post_parent;
+  }
+
   return get_post_meta( $chapter_id, 'fictioneer_chapter_story', true );
 }
 
